a48804b8731435aaafd0ccf35306f6232d224577
Sarthak Tejale <stejale1@stevens.edu>
2024-02-28 18:45:37 -0500

    img change

--- frontend/src/components/Loginpage.js
+++ frontend/src/components/Loginpage.js
@@ -28,7 +28,7 @@ function Loginpage() {
             <div className="row g-0">
               <div className="col-md-6 col-lg-5 d-none d-md-block">
                 <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                  src="foodbasket.png"
                   alt="login form"
                   className="img-fluid"
                   style={{ borderRadius: "1rem 0 0 1rem" }}
