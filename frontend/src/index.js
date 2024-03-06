fad30c9cd0ff63f32f34b13c3db3934f3aa64dcb
ShashankRKumar <63204698+ShashankRKumar@users.noreply.github.com>
2024-03-06 17:56:02 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import {initializeApp} from 'firebase/app'
+import {getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCUKmmMgSYnVJhndSjlbnskp1MZSb_XDEE",
+    authDomain: "fir-db584.firebaseapp.com",
+    // The value of `databaseURL` depends on the location of the database
+    projectId: "fir-db584",
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
+
+export default db;
