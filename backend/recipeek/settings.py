1b9b17482314d3251fc2791af93b19c0b9e4a622
Prakhar564 <pagarwa8@stevens.edu>
2024-03-25 18:30:04 -0400

    database update

--- backend/recipeek/settings.py
+++ backend/recipeek/settings.py
@@ -88,23 +88,14 @@ WSGI_APPLICATION = "recipeek.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'recipeekdb',
-        'USER': 'kevin_admin',
-        'PASSWORD': 'Zz130366!',
-        'HOST': 'recipeekdb-instance.ch2gwsam8xly.us-east-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
