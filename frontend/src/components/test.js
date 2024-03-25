eb6fce941b0a8894f8d815439e083c5e9417a17b
Cornymac <kevin130366@gmail.com>
2024-03-25 16:54:51 -0400

    fix

--- frontend/src/components/test.js
+++ frontend/src/components/test.js
@@ -5,17 +5,11 @@ function Dashboard() {
     const [ingredients, setIngredients] = useState('');
     const [recipes, setRecipes] = useState([]);
 
-    // Spoonacular API key
-    // const apiKey = "4a2351deb18546f4a63a0a7e16d60263";
-    // const apiKey = process.env.REACT_APP_SPOONACULAR_API_KEY;
-    // console.log(process.env.REACT_APP_SPOONACULAR_API_KEY)
-    console.log('OpenAI API Key:', process.env.REACT_APP_OPENAI_API_KEY);
-    console.log('Spoonacular API Key:', process.env.REACT_APP_SPOONACULAR_API_KEY);
 
     const fetchRecipes = async (event) => {
         event.preventDefault();
         try {
-            const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&ingredients=${encodeURIComponent(ingredients)}&number=10`);
+            const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=4a2351deb18546f4a63a0a7e16d60263&ingredients=${encodeURIComponent(ingredients)}&number=10`);
             const data = await response.json();
             setRecipes(data);
         } catch (error) {
