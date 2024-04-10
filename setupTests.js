9b66680fd9e649722e798e0f7fe4dea91a854762
sarthaktejale <stejale1@stevens.edu>
2024-04-10 14:38:21 -0400

    setuptest changes

--- setupTests.js
+++ setupTests.js
@@ -1,6 +1,9 @@
+// Import the necessary libraries
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
+// Configure Enzyme to use the React 16 adapter
 Enzyme.configure({ adapter: new Adapter() });
 
- 
+// Export the Enzyme configuration
+export default Enzyme;
