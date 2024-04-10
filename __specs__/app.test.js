d272a7824eed0696e80b4b8521fcf47be87dd976
sarthaktejale <stejale1@stevens.edu>
2024-04-10 14:52:47 -0400

    chenges

--- __specs__/app.test.js
+++ __specs__/app.test.js
@@ -1,8 +1,8 @@
   import React from 'react';
   import { shallow } from 'enzyme';
-  import App from 'frontend\src\App.js';
+  import App from '../frontend/src/App.js';
   describe('First React component test with Enzyme', () => {
     it('renders without crashing', () => {
     shallow(<App />);
   });
-  });
+  })
