093a5c6c180f0ca07ea27545fb3935501fbf739f
sarthaktejale <stejale1@stevens.edu>
2024-04-10 14:51:04 -0400

    app.test.js changes

--- __specs__/app.test.js
+++ __specs__/app.test.js
@@ -1,6 +1,6 @@
   import React from 'react';
   import { shallow } from 'enzyme';
-  import App from '../components/app';
+  import App from 'frontend\src\App.js';
   describe('First React component test with Enzyme', () => {
     it('renders without crashing', () => {
     shallow(<App />);
