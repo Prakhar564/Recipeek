4ed4ad567fbee79b609f94427dac45464f7edc02
sarthaktejale <stejale1@stevens.edu>
2024-04-17 17:58:22 -0400

    Cypress tests and readme edit

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('First Test', () => {
+  it('Contains correct header text', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('[data-test="Recipeek-header"]').should('contain.text', 'Welcome to Recipeek')
   })
 })
